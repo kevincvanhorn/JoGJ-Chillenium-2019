2a10cb8ea0024626a4d96db36aadb72e488ce0ee
Kevin VanHorn <kevin.vanhorn@utsouthwestern.edu>
2019-10-05 12:32:10 -0500

    UI.

---
+++ JoGJ/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text DisplayText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DisplayText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

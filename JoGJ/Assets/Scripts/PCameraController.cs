e7d1485309719ca5a32ff24957883f1e3428716d
Kevin VanHorn <kevin.vanhorn@utsouthwestern.edu>
2019-10-04 21:02:41 -0500

    Added - Controller.

---
+++ JoGJ/Assets/Scripts/PCameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCameraController : MonoBehaviour
+{
+    public float CamSensitivity;
+    public float CamSmoothing;
+
+    public Rigidbody PlayerRigidbody;
+
+    private Vector2 mouseLook;
+    private Vector2 smoothV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!PlayerRigidbody) return;
+
+        Vector2 CamInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        CamInput = Vector2.Scale(CamInput, new Vector2(CamSensitivity * CamSmoothing, CamSensitivity * CamSmoothing));
+
+        smoothV.x = Mathf.Lerp(smoothV.x, CamInput.x, 1f / CamSmoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, CamInput.y, 1f / CamSmoothing);
+        mouseLook += smoothV;
+
+        // vector3.right means the x-axis
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        PlayerRigidbody.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, PlayerRigidbody.transform.up);
+    }
+}
---
+++ JoGJ/Assets/Scripts/PFirstPersonController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PFirstPersonController : MonoBehaviour
+{
+    // Input Variables:
+
+    // Movement Variables
+    public float PlayerSpeed = 10.0f;
+    protected Vector3 PlayerDirection;
+
+    protected Rigidbody rigidBody;
+    private Vector3 PlayerDirX, PlayerDirZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        // Get Components attached to this object.
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!rigidBody) return;
+
+        PlayerDirZ = Input.GetAxis("Vertical") * rigidBody.transform.forward;
+        PlayerDirX = Input.GetAxis("Horizontal") * rigidBody.transform.right;
+
+        Vector3 velocity = PlayerSpeed * (PlayerDirZ + PlayerDirX);
+        velocity.y = rigidBody.velocity.y;
+
+        rigidBody.velocity = velocity;
+    }
+}
